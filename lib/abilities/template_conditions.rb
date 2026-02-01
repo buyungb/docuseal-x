e72297d9060366c8d845b727da5836966f888d15
dudungk <buyung@aplindo.tech>
2026-02-01 11:39:08 +0700

    fix(abilities): prevent errors with folder access table
    Adds a check for the existence of the 'folder_accesses' table before querying it, preventing potential runtime errors.
    
    Updates the consent checkbox event listener from 'click' to 'change' for better semantic accuracy.

--- lib/abilities/template_conditions.rb
+++ lib/abilities/template_conditions.rb
@@ -33,20 +33,24 @@ module Abilities
     # For editors - can see own templates OR templates shared with them OR templates in shared folders
     def editor_collection(user)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
 
-      Template.where(account_id: user.account_id).where(
-        Template.arel_table[:author_id].eq(user.id)
-          .or(Template.arel_table[:id].in(shared_template_ids.arel))
-          .or(Template.arel_table[:folder_id].in(shared_folder_ids.arel))
-      )
+      base_condition = Template.arel_table[:author_id].eq(user.id)
+                               .or(Template.arel_table[:id].in(shared_template_ids.arel))
+
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+        base_condition = base_condition.or(Template.arel_table[:folder_id].in(shared_folder_ids.arel))
+      end
+
+      Template.where(account_id: user.account_id).where(base_condition)
     end
 
     def editor_entity(template, user:)
       return true if template.account_id.blank?
       return true if template.author_id == user.id && template.account_id == user.account_id
       return true if template.template_accesses.exists?(user_id: user.id)
-      return true if template.folder && FolderAccess.exists?(template_folder_id: template.folder_id, user_id: user.id)
+      return true if folder_access_table_exists? && template.folder &&
+                     FolderAccess.exists?(template_folder_id: template.folder_id, user_id: user.id)
 
       false
     end
@@ -54,19 +58,32 @@ module Abilities
     # For viewers - can only see templates explicitly shared with them OR templates in shared folders
     def viewer_collection(user)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
 
-      Template.where(account_id: user.account_id).where(
-        Template.arel_table[:id].in(shared_template_ids.arel)
-          .or(Template.arel_table[:folder_id].in(shared_folder_ids.arel))
-      )
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+
+        Template.where(account_id: user.account_id).where(
+          Template.arel_table[:id].in(shared_template_ids.arel)
+            .or(Template.arel_table[:folder_id].in(shared_folder_ids.arel))
+        )
+      else
+        Template.where(account_id: user.account_id)
+                .where(Template.arel_table[:id].in(shared_template_ids.arel))
+      end
     end
 
     def viewer_entity(template, user:)
       return true if template.account_id.blank?
       return true if template.template_accesses.exists?(user_id: user.id)
-      return true if template.folder && FolderAccess.exists?(template_folder_id: template.folder_id, user_id: user.id)
+      return true if folder_access_table_exists? && template.folder &&
+                     FolderAccess.exists?(template_folder_id: template.folder_id, user_id: user.id)
 
+      false
+    end
+
+    def folder_access_table_exists?
+      @folder_access_table_exists ||= ActiveRecord::Base.connection.table_exists?('folder_accesses')
+    rescue StandardError
       false
     end
   end
