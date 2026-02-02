59323808eca6e00fb4d14c9ce8a34b8d288bb573
dudungk <buyung@aplindo.tech>
2026-02-02 09:22:05 +0700

    fix: update sms checkbox and folder access conditions
    Adjust the SMS invitation checkbox in the view to use `check_box_tag`
    and correct its name attribute for proper form submission.
    Add a conditional check for the `folder_accesses` table existence
    in abilities to prevent errors if the table is not present.

--- lib/abilities/submission_conditions.rb
+++ lib/abilities/submission_conditions.rb
@@ -8,28 +8,38 @@ module Abilities
     def editor_collection(user)
       own_template_ids = Template.where(author_id: user.id, account_id: user.account_id).select(:id)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
-      templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
 
-      Submission.where(account_id: user.account_id)
-                .where(
-                  Submission.arel_table[:template_id].in(own_template_ids.arel)
-                    .or(Submission.arel_table[:template_id].in(shared_template_ids.arel))
-                    .or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
-                )
+      base_condition = Submission.arel_table[:template_id].in(own_template_ids.arel)
+                                 .or(Submission.arel_table[:template_id].in(shared_template_ids.arel))
+
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+        templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+        base_condition = base_condition.or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
+      end
+
+      Submission.where(account_id: user.account_id).where(base_condition)
     end
 
     # For viewers - can only see submissions for templates shared with them OR templates in shared folders
     def viewer_collection(user)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
-      templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
 
-      Submission.where(account_id: user.account_id)
-                .where(
-                  Submission.arel_table[:template_id].in(shared_template_ids.arel)
-                    .or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
-                )
+      base_condition = Submission.arel_table[:template_id].in(shared_template_ids.arel)
+
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+        templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+        base_condition = base_condition.or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
+      end
+
+      Submission.where(account_id: user.account_id).where(base_condition)
+    end
+
+    def folder_access_table_exists?
+      @folder_access_table_exists ||= ActiveRecord::Base.connection.table_exists?('folder_accesses')
+    rescue StandardError
+      false
     end
   end
 end
--- lib/abilities/submitter_conditions.rb
+++ lib/abilities/submitter_conditions.rb
@@ -8,15 +8,18 @@ module Abilities
     def editor_collection(user)
       own_template_ids = Template.where(author_id: user.id, account_id: user.account_id).select(:id)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
-      templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+
+      base_condition = Submission.arel_table[:template_id].in(own_template_ids.arel)
+                                 .or(Submission.arel_table[:template_id].in(shared_template_ids.arel))
+
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+        templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+        base_condition = base_condition.or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
+      end
 
       submission_ids = Submission.where(account_id: user.account_id)
-                                 .where(
-                                   Submission.arel_table[:template_id].in(own_template_ids.arel)
-                                     .or(Submission.arel_table[:template_id].in(shared_template_ids.arel))
-                                     .or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
-                                 )
+                                 .where(base_condition)
                                  .select(:id)
 
       Submitter.where(submission_id: submission_ids)
@@ -25,17 +28,26 @@ module Abilities
     # For viewers - can only see submitters for templates shared with them OR templates in shared folders
     def viewer_collection(user)
       shared_template_ids = TemplateAccess.where(user_id: user.id).select(:template_id)
-      shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
-      templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+
+      base_condition = Submission.arel_table[:template_id].in(shared_template_ids.arel)
+
+      if folder_access_table_exists?
+        shared_folder_ids = FolderAccess.where(user_id: user.id).select(:template_folder_id)
+        templates_in_shared_folders = Template.where(folder_id: shared_folder_ids, account_id: user.account_id).select(:id)
+        base_condition = base_condition.or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
+      end
 
       submission_ids = Submission.where(account_id: user.account_id)
-                                 .where(
-                                   Submission.arel_table[:template_id].in(shared_template_ids.arel)
-                                     .or(Submission.arel_table[:template_id].in(templates_in_shared_folders.arel))
-                                 )
+                                 .where(base_condition)
                                  .select(:id)
 
       Submitter.where(submission_id: submission_ids)
+    end
+
+    def folder_access_table_exists?
+      @folder_access_table_exists ||= ActiveRecord::Base.connection.table_exists?('folder_accesses')
+    rescue StandardError
+      false
     end
   end
 end
